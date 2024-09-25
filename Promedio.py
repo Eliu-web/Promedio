42dd5d40279dca0ac8ce14e1ab94c5b09136808e
Eliu-web <a.23308051281276@cbtis128.edu.mx>
2024-09-24 19:56:46 -0600

    Add files via upload

---
+++ Promedio.py
@@ -0,0 +1,11 @@
+#En esta actvidad realizaremos un codigo que nos diga la cantidad de gasolina gastada
+
+num1 = int(input("Introduce un numero: ")) #Nos pide introducir un valor
+
+num2 = int(input("Introduce un numero: ")) #Nos pide introducir un valor
+
+num3 = int(input("Introduce un numero: ")) #Nos pide introducir un valor
+
+promedio = (num1+num2+num3) / 3 #Indica la operacion a realizar
+
+print ("El promedio de los numeros es: ", promedio) #El resultado final
